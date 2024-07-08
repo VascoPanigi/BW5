fe053c2a436675fd106b08ad45c5fb72d42959cb
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-08 10:21:18 +0200

    repo setup

---
+++ src/test/java/team6/BW5/Bw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package team6.BW5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
