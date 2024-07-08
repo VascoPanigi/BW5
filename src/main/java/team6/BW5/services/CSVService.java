491a9edcb9267bf18853b944d8453a6ed7d727c2
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-08 18:18:49 +0200

    adds csvReader working
    Need to fix municipalities

--- src/main/java/team6/BW5/services/CSVService.java
+++ src/main/java/team6/BW5/services/CSVService.java
@@ -1,5 +1,8 @@
 package team6.BW5.services;
 
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -11,15 +14,33 @@ import team6.BW5.repositories.ProvinceRepository;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
     @Service
     public class CSVService {
 
-        @Autowired
-        private ProvinceRepository provinceRepository;
+        public List<String[]> convertCSV(Path path) throws IOException {
+            try (Reader reader = Files.newBufferedReader(path);
+                 CSVReader csvReader = new CSVReaderBuilder(reader)
+                         .withCSVParser(new CSVParserBuilder()
+                                 .withSeparator(';')
+                                 .build())
+                         .withSkipLines(1)
+                         .build()) {
+                return csvReader.readAll();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw e;
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new IOException("Error reading CSV file: " + e.getMessage(), e);
+            }
+        }
 
     }
 
---
+++ src/main/java/team6/BW5/utility/DBInitializer.java
@@ -0,0 +1,69 @@
+package team6.BW5.utility;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import team6.BW5.entities.Municipality;
+import team6.BW5.entities.Province;
+import team6.BW5.repositories.MunicipalityRepository;
+import team6.BW5.repositories.ProvinceRepository;
+import team6.BW5.services.CSVService;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DBInitializer {
+
+    @Autowired
+    private  CSVService csvService;
+    @Autowired
+    private ProvinceRepository provinceRepository; 
+    
+    @Autowired
+    private MunicipalityRepository municipalityRepository;
+
+    @Bean
+    public void initializeProvinces() throws IOException{
+        Path provincesPath = Paths.get("src/main/java/team6/BW5/data/province-italiane.csv");
+
+        List<String[]> provincesList = csvService.convertCSV(provincesPath);
+
+        List<Province> newProvinces = provincesList.stream()
+                .map(row -> {
+                    Province province = new Province();
+                    province.setNotation(row[0]);
+                    province.setName(row[1]);
+                    province.setRegion(row[2]);
+                    return province;
+                })
+                .toList();
+
+        newProvinces.forEach(provinceRepository::save);
+
+    }
+
+    @Bean
+    public void initializeMunicipalities() throws IOException{
+        Path provincesPath = Paths.get("src/main/java/team6/BW5/data/comuni-italiani.csv");
+
+        List<String[]> municipalitiesList = csvService.convertCSV(provincesPath);
+
+        List<Municipality> newMunicipalities = municipalitiesList.stream()
+                .map(row -> {
+                    Municipality municipality = new Municipality();
+                    municipality.setName(row[2]);
+                    municipality.setProvince(provinceRepository.findByName(row[3]));
+                    return municipality;
+                })
+                .toList();
+
+        newMunicipalities.forEach(municipalityRepository::save);
+
+
+    }
+}
