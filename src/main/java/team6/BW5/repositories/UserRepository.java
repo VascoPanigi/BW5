c37a6b297fbbde3da4240e563cc4623d20f016f9
Jacopo De maio <jajodemaio@gmail.com>
2024-07-09 09:55:10 +0200

    Refactor drawSQL diagram

--- src/main/java/team6/BW5/repositories/UserRepository.java
+++ src/main/java/team6/BW5/repositories/UserRepository.java
@@ -2,10 +2,10 @@ package team6.BW5.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import team6.BW5.entities.Municipality;
+import team6.BW5.entities.User;
 
 import java.util.UUID;
 
 @Repository
-public interface MunicipalityRepository extends JpaRepository<Municipality, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
