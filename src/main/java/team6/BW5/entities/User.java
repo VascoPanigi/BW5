45c425513dbc06cf5fd95bf327198f06e2455f83
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-07-09 09:55:20 +0200

    Add Security

--- src/main/java/team6/BW5/entities/User.java
+++ src/main/java/team6/BW5/entities/User.java
@@ -1,11 +1,15 @@
 package team6.BW5.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +19,9 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @ToString
-public class User {
+@JsonIgnoreProperties({"password", "enabled", "authorities"})
+
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue
@@ -24,6 +30,8 @@ public class User {
     private String username;
 
     private String email;
+
+    private String password;
 
     private String name;
 
@@ -38,13 +46,23 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> rolesList;
 
-    public User(String username, String email, String name, String surname, String avatarURL, List<Role> rolesList) {
+    public User(String username, String email, String password, String name, String surname, String avatarURL) {
         this.username = username;
         this.email = email;
+        this.password = password;
         this.name = name;
         this.surname = surname;
         this.avatarURL = avatarURL;
-        this.rolesList = rolesList;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
     }
 }
 
