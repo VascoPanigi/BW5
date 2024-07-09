be86256cb80d38511239843b325b48889bcf9109
Francesco Cossu <cossufrancesco95@gmail.com>
2024-07-09 12:33:21 +0200

    updated Address

--- src/main/java/team6/BW5/entities/Address.java
+++ src/main/java/team6/BW5/entities/Address.java
@@ -1,7 +1,7 @@
 package team6.BW5.entities;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,17 +13,19 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue
     private UUID id;
     private String street;
     private String zipCode;
+    @ManyToOne
+    @JoinColumn(name = "municipality")
     private Municipality municipality_id;
-    private Province province;
 
 
-    public Address(String street, String zipCode, Municipality municipality_id, Province province) {
+    public Address(String street, String zipCode, Municipality municipality_id) {
         this.street = street;
         this.zipCode = zipCode;
         this.municipality_id = municipality_id;
-        this.province = province;
     }
 }
