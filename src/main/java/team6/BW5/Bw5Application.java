a058df203d28b59acdc6967e630b4c6296495159
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-07-09 09:25:42 +0200

    Add Errors and excepetion

--- src/main/java/team6/BW5/Bw5Application.java
+++ src/main/java/team6/BW5/Bw5Application.java
@@ -1,36 +1,18 @@
 package team6.BW5;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.io.input.BOMInputStream;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import team6.BW5.entities.Province;
-import team6.BW5.services.CSVService;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Bw5Application {
 
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(Bw5Application.class, args);
+        SpringApplication.run(Bw5Application.class, args);
 
 
-	}
-
+    }
 
 
 }
