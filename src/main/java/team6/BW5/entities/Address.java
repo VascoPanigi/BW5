80ed641e7a219f88011c301fe7aa079305ba92dc
Jacopo De maio <jajodemaio@gmail.com>
2024-07-09 11:55:18 +0200

    @Id and@GenrationValue in AdressClass

--- src/main/java/team6/BW5/entities/Address.java
+++ src/main/java/team6/BW5/entities/Address.java
@@ -2,6 +2,10 @@ package team6.BW5.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,10 +13,14 @@ import lombok.Setter;
 import java.util.UUID;
 
 @Entity
+@Table(name = "address")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
     private UUID id;
     private String street;
     private String zipCode;
