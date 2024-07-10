bf194a1f78f3fe2a3fcf7bd54adf96024de78f25
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-07-10 15:34:58 +0200

    adds simplegrantedauthority

--- src/main/java/team6/BW5/entities/User.java
+++ src/main/java/team6/BW5/entities/User.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -68,7 +69,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority(this.rolesList.toString()));
     }
 
 }
